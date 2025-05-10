961231ed0585f2ffb15845c8db0c7f3471c48f5f
rohittt1 <rohit.rthd.04@gmail.com>
2025-05-10 06:08:43 +0530

    imagekit-auth routes with middleware authentication

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { auth } from "@clerk/nextjs/server";        // we will be using this auth on the backend part, and that's why it needs to come with the server.
+import { NextResponse } from "next/server";
+import ImageKit from "imagekit";
+import { error } from "console";
+
+
+// from imagekit documentation
+// SDK initialization
+
+// var ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
+    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || "",
+});
+
+export async function GET() {
+    // in the nextJS, there was a vulnerability, not in the newer version (which i am currently building on), but the previous one.
+    // security vulnerability in nextJS -> if there is any endpoint, that endpoint and whole middleware thing can be bypassed.
+    // so the dev team of nextJS- suggested/mentioned that the every single endpoint, you should actually authenticate, whether the user is authenticated to access that endpoint, whether it is validated by middleware or not. 
+
+    try {
+        const {userId} = await auth()
+        if(!userId){
+            return NextResponse.json({error: "Unauthorized"},
+                {status: 401}
+            )
+        }
+    
+        
+        const authParams = imagekit.getAuthenticationParameters()
+        
+        return NextResponse.json(authParams)
+    } catch (error) {
+        return NextResponse.json({error: "Failed to generate authentication parameters for imagekit"}, {status: 500})
+    }
+    
+
+} 
---
+++ app/api/upload/route.ts
@@ -0,0 +1,11 @@
+// incase, we be using any video/audio component -> we have to make sure that we are using provider ->so that the application is always authenticated
+
+//  IMPORTANT:- 
+// How do we upload any data? --> whenever any data is uploaded, we use the imagekit component and the image/video goes inside the imagekit component
+// --> because the endpoint automatically hits it
+// --> it uploads that image/video and 
+// --> returns back the body 
+
+// in this file -> we only gonna work on route
+// our job in the upload is that take the data (userID and image) from the frontend -> take it onto the upload endpoint and save it in the database.
+
